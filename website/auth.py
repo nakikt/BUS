3fba79aa5bc16841a745523cbb733950129d34f7
nakikt <NKlucznik@gmail.com>
2023-01-15 14:50:14 +0100

    Logowanie

--- website/auth.py
+++ website/auth.py
@@ -4,17 +4,19 @@ from . import db
 from .models import User
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import pyqrcode
+import pyotp, qrcode
 from io import BytesIO
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-
+    new_user = User(username="a", password=generate_password_hash("12345", method='sha256'), otp=False)
+    db.session.add(new_user)
     # db.session.commit()
     # login_user(new_user, remember=True)
     if request.method == 'POST':
+
         username = request.form.get("username")
         password = request.form.get("password")
 
@@ -22,10 +24,14 @@ def login():
         if user:
             if user:
 
-                if check_password_hash(user.password, password) and user.verify_totp(otp):
+                if check_password_hash(user.password, password): # and user.verify_totp(otp):
                     flash("Logged in!", category='success')
                     login_user(user, remember=True)
-                    return redirect(url_for('views.home'))
+                    return render_template('two-factor-setup.html', user=current_user), 200, {
+                        'Cache-Control': 'no-cache, no-store, must-revalidate',
+                        'Pragma': 'no-cache',
+                        'Expires': '0'}
+                    #return redirect(url_for('views.home'))
 
                 else:
                     flash('Password is incorrect.', category='error')
@@ -48,10 +54,10 @@ def firstlogin():
             if check_password_hash(user.password, password):
                 flash("Logged in!", category='success')
                 login_user(user, remember=True)
-                return render_template("two-factor-setup.html"), 200, {
-                        'Cache-Control': 'no-cache, no-store, must-revalidate',
-                        'Pragma': 'no-cache',
-                        'Expires': '0'}
+                return render_template('two-factor-setup.html', user= current_user), 200, {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
             else:
                 flash('Invalid username, password.', category='error')
         else:
@@ -64,14 +70,26 @@ def logout():
     logout_user()
     return redirect(url_for("views.home"))
 
-
-@auth.route('/qrcode')
+@auth.route('/twofactor')
 @login_required
-def qrcode():
+def two_factor_setup():
+
+    # since this page contains the sensitive qrcode, make sure the browser
+    # does not cache it
+    return render_template('two-factor-setup.html', user = current_user), 200, {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
+@auth.route('/qr')
+@login_required
+def qr():
+    print(current_user)
     user = current_user
-    print(user)
     # render qrcode for FreeTOTP
-    url = pyqrcode.create(user.get_totp_uri())
+    url = qrcode.make(user.get_totp_uri())
+    print(url)
+    qrcode.make(user.get_totp_uri()).save("website/code.png")
     stream = BytesIO()
     url.svg(stream, scale=5)
     return stream.getvalue(), 200, {
